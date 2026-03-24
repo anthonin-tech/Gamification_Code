35b711dd3e69f25f585e9fd4a50b2c6c38c49aa0
Anthonin <anthonin.chesneau08@gmail.com>
2026-03-24 16:11:38 +0100

    ajout de la structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-container">
+            <router-link to="/" class="navbar-home">CodeQuest</router-link>
+            <router-link to="/language" class="navbar-language">Language</router-link>
+            <router-link to="/progression" class="navbar-progression">Progression</router-link>
+            <router-link to="/profil" class="navbar-profil">Profil</router-link>
+        </div>
+    </nav>
+</template>
+
+<style scoped src="../assets/styles/navbar.css"></style>
---
+++ src/views/Language.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="page">
+    <h1 class="page-title">Language</h1>
+  </main>
+</template>
